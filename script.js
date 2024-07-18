2e1d563ed7ca56fe665f4a67dca11220d4459227
Aiken <g.e.omonova@gmail.com>
2024-07-18 11:49:52 +0300

    homework

---
+++ script.js
@@ -0,0 +1,101 @@
+let firstUserName = "Nooruz";
+console.log(firstUserName);
+let secondUserName = "Zhakshylyk";
+console.log(secondUserName);
+let thirdUserName = "Alisher";
+console.log(thirdUserName);
+let fourthUserName = "elizar";
+console.log(fourthUserName);
+
+let firstUserPoint = 97;
+console.log(firstUserPoint);
+
+let secondUserPoint = 89;
+console.log(secondUserPoint);
+
+let thirdUserPoint = 75;
+console.log(thirdUserPoint);
+
+let fourthUserPoint = 90;
+console.log(fourthUserPoint);
+
+let Nooruz1 = "Nooruz-";
+let Nooruz2 = "97 баллов из 100";
+let result = Nooruz1 + Nooruz2;
+console.log(result);
+
+let Zhakshylyk1 = "Zhakshylyk-";
+let Zhakshylyk2 = "98 баллов из 100";
+let result2 = Zhakshylyk1 + Zhakshylyk2;
+console.log(result2);
+
+let Alisher1 = "Alisher-";
+let Alisher2 = "75 баллов из 100";
+let result3 = Alisher1 + Alisher2;
+console.log(result3);
+
+let Elizar1 = "Elizar-";
+let Elizar2 = "90 баллов из 100";
+let result4 = Elizar1 + Elizar2;
+console.log(result4);
+
+let a = 1;
+let b = 4;
+let c = a * b;
+console.log(`${a}*${b}=${c}`);
+
+let aa = 2;
+let bb = 4;
+let cc = aa * bb;
+console.log(`${aa}*${bb}=${cc}`);
+
+let aaa = 3;
+let bbb = 4;
+let ccc = aaa * bbb;
+console.log(`${aaa}*${bbb}=${ccc}`);
+
+let a1 = 4;
+let b1 = 4;
+let c1 = a1 * b1;
+console.log(`${a1}*${b1}=${c1}`);
+
+let a12 = 5;
+let b12 = 4;
+let c12 = a12 * b12;
+console.log(`${a12}*${b12}=${c12}`);
+
+let a1a = 6;
+let b1b = 4;
+let c1c = a1a * b1b;
+console.log(`${a1a}*${b1b}=${c1c}`);
+
+let aa1 = 7;
+let bb1 = 4;
+let cc1 = aa1 * bb1;
+console.log(`${aa1}*${bb1}=${cc1}`);
+
+let a2 = 8;
+let b2 = 4;
+let c2 = a2 * b2;
+console.log(`${a2}*${b2}=${c2}`);
+
+let a3 = 9;
+let b3 = 4;
+let c3 = a3 * b3;
+console.log(`${a3}*${b3}=${c3}`);
+
+let a4 = 10;
+let b4 = 4;
+let c4 = a4 * b4;
+console.log(`${a4}*${b4}=${c4}`);
+
+alert("Вы успешно зарегистрировались!");
+
+let text = confirm(
+  "При перезагрузке сайта, введённые данные на форме будут очищены, Вы уверены что хотите перезагрузить?"
+);
+alert(text);
+
+let myName = prompt("Введите ваше имя", "Aiken");
+
+console.log(myName);
